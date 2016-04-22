80c9085dc893736383494bafe639603809f2ff23
nylux <l.stanesa@openmailbox.org>
2016-04-21 20:42:39 -0400

    implemented simple database backbone

--- wickr.py
+++ wickr.py
@@ -1,14 +1,20 @@
 from flask import Flask, request, session, render_template, g, url_for, redirect
+from markdown import markdown
 import os
+
+from db import Database
 
 app = Flask(__name__)
 
 app.config.update(dict(
     DATABASE = os.path.join(app.root_path, 'wickr.db'),
     DEBUG = True,
-
+    SITE_TITLE = 'Wickr',
+    JSON_API = True
 ))
 app.config.from_envvar('WICKR_SETTINGS', silent=True)
+
+g.db = Database(app.config['DATABASE'])
 
 @app.route('/')
 def route_index():
@@ -17,3 +23,7 @@ def route_index():
 @app.route('/wiki/<path:article>')
 def route_article(article):
     return render_template('article.html', article=article)
+
+@app.template_filter('markdown')
+def filter_markdown(s):
+    return markdown(s)
